27ced430c545b7b2ac8b8e3d0002c6a207ff832f
saltwatercowboy <gustiv9@gmail.com>
2023-11-26 17:30:58 +0000

    all initial files

---
+++ src/components/Landing-page.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class='header'>
+        <img src="/src/assets/logo.png" alt="logo" />
+        <h1 id="headname">Rootstalk</h1>
+        <h1 id="headtext" href="localhost:5173">Home</h1>
+    </div>
+    <v-stage :config="boardSpace">
+        <v-layer>
+            <v-rect :config="background"></v-rect>
+        </v-layer>
+    </v-stage>
+</template>
+
+<script>
+
+
+</script>
