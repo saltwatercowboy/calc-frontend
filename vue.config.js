698c6c563339d7142506e743da94758d48f26051
Ernst Gustav von Weyhausen <eg.vw@Laptop.local>
2023-12-13 22:03:35 +0000

    vue config issues

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
+import vue from '@vitejs/plugin-vue'
+
+vue()
+
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/calc/" : "/",
+    publicPath: process.env.NODE_ENV === "production" 
+    ? "/calc/" 
+    : "/"
   };
