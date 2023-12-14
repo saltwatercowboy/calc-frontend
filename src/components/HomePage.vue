1435a84e2e1fe0752a40b7852c4ccebd04bd3e76
Ernst Gustav von Weyhausen <eg.vw@Laptop.local>
2023-12-14 23:30:19 +0000

    testing about-l sizing

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -495,7 +495,7 @@ main {
     width: 50%;
     min-width: 50%;
     height: 100%;
-    max-height: 150%;
+    max-height: fit-content;
     box-shadow: 2px 0px 10px rgba(0,0,0,.2);
     justify-content: center;
     align-items: center;
